9e07e311c5d9c8dd9c02e8c43049143a8706c5de
SkyeBlack <296471989@qq.com>
2019-09-03 16:32:25 +0800

    change version to v1.0.2

--- packages/bhp-core/src/consts.ts
+++ packages/bhp-core/src/consts.ts
@@ -79,7 +79,7 @@ export const BRC_HEADER = "0142";
 
 export const BRC_FLAG = "e0";
 
-export const RPC_VERSION = "1.0.1";
+export const RPC_VERSION = "1.0.2";
 
 export const TX_VERSION: { [key: string]: number } = {
   CLAIM: 0,
